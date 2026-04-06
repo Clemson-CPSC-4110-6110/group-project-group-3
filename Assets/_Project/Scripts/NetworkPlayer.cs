3520dd506a50a18936c53949071e3b562374d4ff
Diana Sanchez <dianasanchezcruz17@gmail.com>
2026-04-05 23:50:31 -0400

    changed script to move with body

--- Assets/_Project/Scripts/NetworkPlayer.cs
+++ Assets/_Project/Scripts/NetworkPlayer.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 
 public class NetworkPlayer : NetworkBehaviour
 {
+    [Header("Avatar Bones")]
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
 
-    public override void Spawned()
+    [Header("XR Rig References")]
+    public Transform xrHead;
+    public Transform xrLeftHand;
+    public Transform xrRightHand;
+
+    public override void FixedUpdateNetwork()
     {
         if (Object.HasInputAuthority)
         {
-            Debug.Log("This is MY player");
+            head.position = xrHead.position;
+            head.rotation = xrHead.rotation;
+
+            leftHand.position = xrLeftHand.position;
+            leftHand.rotation = xrLeftHand.rotation;
+
+            rightHand.position = xrRightHand.position;
+            rightHand.rotation = xrRightHand.rotation;
         }
     }
 }
